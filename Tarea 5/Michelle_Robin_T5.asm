3eeabd1cab8abfa894609dc904b2c9ec398ea0a4
robz25 <weiserfi@outlook.com>
2021-02-08 17:11:32 -0600

    subrutinas T4

--- Tarea 5/Michelle_Robin_T5.asm
+++ Tarea 5/Michelle_Robin_T5.asm
@@ -149,9 +149,9 @@ Msg2_L2:                 fcc     "  AcmPQ  CUENTA"
         MOVB #$17,RTICTL
         
         ;PTH
-         CLR DDRH
-         BSET PIEH,$0F
-         BSET PPSH,$0F   ;interrupcion en flanco creciente
+        CLR DDRH
+        BSET PIEH,$0F
+        BSET PPSH,$0F   ;interrupcion en flanco creciente
         
         ;OC4
         BSET TSCR1,$90  ;encendemos Timer, TFFCLA
@@ -192,8 +192,13 @@ loopIniDsp:
 ;                             PROGRAMA PRINCIPAL
 ;*******************************************************************************
 
-        BRA *
+main:
 
+        BRSET Banderas,$04,main ;salta a main si el bits 2 (%0000 0100) es 1 en Banderas
+        JSR Tarea_Teclado       ;ir a subrutina Tarea_Teclado
+        BRA main        ;salta siempre a main
+
+        BRA *
 
 ;*******************************************************************************
 ;                                     SUBRUTINAS
@@ -302,7 +307,7 @@ Delay:                  ;                 Subrutina Delay
         RTS
 
 ;*******************************************************************************
-;                         SUBRUTINAS DE INTERRUPCION
+;                        SUBRUTINAS DE INTERRUPCION
 ;*******************************************************************************
 
 RTI_ISR:                ;                Subrutina RTI_ISR
@@ -337,3 +342,153 @@ retorno_OC4:
         ADDD TCNT       ;30 + contador en D
         STD TC4         ;30 + contador en TC4
         RTI
+        
+
+;*******************************************************************************
+;                           SUBRUTINAS DE TAREA 4
+;*******************************************************************************
+
+                        ;*******************************************************
+Tarea_Teclado:          ;                      SUBRUTINA
+                        ;*******************************************************
+                        ;Verifica estado de teclas ingresadas
+                           ;y llama a las otras subrutinas
+                        ;*******************************************************
+;        MOVB #$01,PORTB  ;debugging
+;        INC Print+1
+        TST Cont_Reb
+        BNE Retorno_Tarea_Teclado
+        JSR Mux_Teclado
+        BRSET Tecla,$FF,revisar_TCL_LISTA
+
+        BRSET Banderas,$02,revisar_teclas
+        MOVB Tecla,Tecla_IN
+        BSET Banderas,$02
+        MOVB #10,Cont_Reb
+
+        BRA Retorno_Tarea_Teclado
+
+revisar_teclas:
+        LDAA Tecla
+        CMPA Tecla_IN
+        BNE error_al_leer
+        BSET Banderas,$01
+        BRA Retorno_Tarea_Teclado
+
+error_al_leer:
+        MOVW #$FFFF,Tecla ;Pone FF en las posiciones Tecla y Tecla_IN;
+        BCLR Banderas,$03
+        BRA Retorno_Tarea_Teclado
+
+revisar_TCL_LISTA:
+        BRCLR Banderas,$01,Retorno_Tarea_Teclado
+        BCLR Banderas,$03
+        JSR Formar_Array
+
+Retorno_Tarea_Teclado:
+        RTS
+
+                        ;*******************************************************
+Mux_Teclado:            ;                      SUBRUTINA
+                        ;*******************************************************
+                        ;Lee teclado matricial
+                        ;*******************************************************
+;        MOVB #$02,PORTB  ;debugging
+;        INC Print+2
+        LDX #Teclas
+        CLRA
+        MOVB #$EF,Patron
+loop_mux:
+        MOVB Patron,PORTA
+        BRCLR PORTA,$02,tecla_presionada
+        INCA
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        BRCLR PORTA,$04,tecla_presionada
+        INCA
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        BRCLR PORTA,$08,tecla_presionada
+        INCA
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        LSL Patron
+        BRCLR Patron,$F0,nada_presionado
+        BRA loop_mux
+
+nada_presionado:
+;        Inc Print+6
+        MOVB #$FF,Tecla
+        RTS
+
+tecla_presionada:
+        MOVB A,X,Tecla
+;        Inc Print+7
+        RTS
+
+                        ;*******************************************************
+Formar_Array:           ;                      SUBRUTINA
+                        ;*******************************************************
+                        ;Llena arreglo Num_Array con teclas leidas
+                        ;*******************************************************
+;        MOVB #$04,PORTB  ;debugging
+;        INC Print+3
+        Ldx #Num_Array   ; Cargar dirección de Num_Array en el índice Y
+        Ldaa #$0B
+        Ldab Cont_TCL
+        ;Incb
+        Cmpb MAX_TCL
+        Beq full_array
+        Ldab #$0E
+        TST Cont_TCL
+        Beq primer_input
+        Cmpa Tecla_IN
+        Beq reducir_contador
+        Cmpb Tecla_IN
+        Beq poner_array_ok
+        Ldab Cont_TCL
+        Movb Tecla_IN,b,x ; No sabemos si está bien
+        Inc Cont_TCL
+        Bra Nodo_Final
+
+poner_array_ok:
+        Bset Banderas,$04 ;Poner en 1 el bit 3 (Array_Ok)
+        Clr Cont_TCL
+        Bra Nodo_Final
+
+reducir_contador:
+        Dec Cont_TCL
+        Ldab Cont_TCL
+        Movb #$FF,b,x
+        Bra Nodo_Final
+
+primer_input:
+        Cmpa Tecla_IN
+        Beq Nodo_Final
+        Cmpb Tecla_IN
+        Beq Nodo_Final
+        Ldab Cont_TCL
+        Movb Tecla_IN,b,x
+        Inc Cont_TCL
+        Bra Nodo_Final
+
+full_array:
+        Ldab #$0E
+        Cmpa Tecla_IN
+        beq reducir_contador
+        Cmpb Tecla_IN
+        Beq poner_array_ok
+        Bra Nodo_Final
+
+Nodo_Final:
+        Movb #$FF,Tecla_IN
+        RTS
